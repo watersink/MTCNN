66b62ecafbe61ffd5f3ba9f3cf1c6f3cdeedd463
watersink <492034746@qq.com>
2019-04-20 21:04:06 +0800

    delete test

--- FCFacedet_Tester/FCFacedet_Tester/FCFacedet_Tester/FCFacedet_Tester.cpp
+++ FCFacedet_Tester/FCFacedet_Tester/FCFacedet_Tester/FCFacedet_Tester.cpp
@@ -20,7 +20,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <fstream>
 #include "head.h"
 using namespace caffe;
