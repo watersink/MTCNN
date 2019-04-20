fbafd45b225fafe04f762b6e38b8b22821226670
watersink <492034746@qq.com>
2019-04-20 21:18:18 +0800

    add platform deicde

--- FCFacedet_Tester/FCFacedet_Tester/FCFacedet_Tester/head.h
+++ FCFacedet_Tester/FCFacedet_Tester/FCFacedet_Tester/head.h
@@ -12,10 +12,10 @@
 #include <caffe/layers/lrn_layer.hpp>
 #include <caffe/layers/softmax_layer.hpp> 
 
-
+#include "osplatformutil.h"
 namespace caffe
 {
-
+#if defined I_OS_WIN32
 	extern INSTANTIATE_CLASS(InputLayer);
 	extern INSTANTIATE_CLASS(InnerProductLayer);
 	extern INSTANTIATE_CLASS(DropoutLayer);
@@ -34,5 +34,5 @@ namespace caffe
 	extern INSTANTIATE_CLASS(PReLULayer);
 	//REGISTER_LAYER_CLASS(PReLU);
 	extern INSTANTIATE_CLASS(SplitLayer);
-		
+#endif		
 }
